127f666232ab6cc4d85bd90a0407b2793511aef2
freetimezp <kraftukrnet@ukr.net>
2023-04-21 21:07:42 +0300

    13. js-e-commerce-04. Sort data by filters.

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -3,84 +3,20 @@ import React from 'react';
 import Card from '../Card/Card';
 
 import './List.scss';
+import useFetch from '../../hooks/useFetch';
 
-const data = [
-    {
-        id: 1,
-        img: '/img/featured-1.jpg',
-        img2: '/img/featured-4.jpg',
-        title: 'Hat',
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 2,
-        img: '/img/featured-2.jpg',
-        img2: '/img/featured-3.jpg',
-        title: 'Hat',
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 3,
-        img: '/img/featured-3.jpg',
-        img2: '/img/featured-1.jpg',
-        title: 'Hat',
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 4,
-        img: '/img/featured-4.jpg',
-        img2: '/img/featured-2.jpg',
-        title: 'Hat',
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 5,
-        img: '/img/featured-1.jpg',
-        img2: '/img/featured-4.jpg',
-        title: 'Hat',
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 6,
-        img: '/img/featured-2.jpg',
-        img2: '/img/featured-3.jpg',
-        title: 'Hat',
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 7,
-        img: '/img/featured-3.jpg',
-        img2: '/img/featured-1.jpg',
-        title: 'Hat',
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 8,
-        img: '/img/featured-4.jpg',
-        img2: '/img/featured-2.jpg',
-        title: 'Hat',
-        oldPrice: 19,
-        price: 12,
-    },
-];
+const List = ({ catId, maxPrice, sort, subCats }) => {
+    const { data, loading, error } = useFetch(`/products?populate=*&[filters][categories][id]=${catId}${subCats?.map((item) => `&[filters][sub_categories][id][$eq]=${item}`)}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}
+    `);
 
-const List = () => {
     return (
         <div className='list'>
-            {/* {data?.map((item) => (
-                <Card item={item} key={item.id} />
-            ))} */}
+            {loading
+                ? "Wait, loading..."
+                : data?.map((item) => (
+                    <Card item={item} key={item.id} />
+                ))
+            }
         </div>
     );
 }
